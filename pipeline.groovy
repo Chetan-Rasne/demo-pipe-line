a4786fcabf7ba2b51b5f0a0bc52130d67fc14c7a
Chetan-mysterio <chetanrasane7921@gmail.com>
2026-08-14 16:29:54 +0530

    three-step

--- pipeline.groovy
+++ pipeline.groovy
@@ -13,7 +13,12 @@ pipeline {
         }
         stage ("Test"){
             steps {
-                echo 'Test success'
+                sh ''' /opt/maven/bin/mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+  		-Dsonar.projectKey=student-b42 \
+  		-Dsonar.projectName='student-b42' \
+  		-Dsonar.host.url=http://15.206.116.70:9000 \
+  		-Dsonar.token=sqp_c9482d0d6b12aeca88eb0dac1fa230dcf3230590'''
+
             }
         }
         stage ("Deploy"){
